9cefcfcbdb6d572f6f39c5e4c983ad59fd3a89bb
ricardourvina <rurvinac@estudiante.uc.cl>
2026-03-20 00:28:22 -0300

    Revert " Clase 05 WHILE"
    This reverts commit f9b9ef8b886408d8fa893459f41054755fb1a2cf.

--- clases/bucles_while/ejercicio1.py
+++ clases/bucles_while/ejercicio1.py
@@ -1,5 +1,14 @@
-numero = 1
+# Ejercicio 1: Cuenta Ascendente
+# Escribe un programa que utilice un bucle while para contar de 1 a 10 e imprima cada número.
 
-while numero <= 10:
-    print(numero)
-    numero += 1
+# Inicializar la variable contador
+contador = 1
+# Iniciar el bucle while
+while contador <= 10:
+    # Imprimir el número
+    print(contador)
+    # Incrementar el contador
+    contador += 1
+
+# Imprimir mensaje de fin
+print("Fin del programa")
