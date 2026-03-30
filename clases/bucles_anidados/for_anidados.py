c88790c665e576e4d3814539861b49fecd9db853
ricardourvina <rurvinac@estudiante.uc.cl>
2026-03-29 22:00:40 -0300

    Clase 08 condicionales anidados

--- clases/bucles_anidados/for_anidados.py
+++
@@ -1,3 +0,0 @@
-for i in range(1, 10, 2):
-    for j in range(3):
-        print(f"i: {i}, j: {j}")
--- clases/bucles_anidados/while_anidados.py
+++
@@ -1,8 +0,0 @@
-
-n = 1
-while n < 10:
-    m = 0
-    while m < 5:
-        print(f"n: {n}, m: {m}")
-        m += 1
-    n += 2
