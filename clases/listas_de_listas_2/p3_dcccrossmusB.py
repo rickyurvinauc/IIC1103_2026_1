76fdf5a4d4773cc94b2bfed97deb304ff3f6a6e6
ricardourvina <rurvinac@estudiante.uc.cl>
2026-05-03 19:28:30 -0400

    Clase 20 recursion 2

--- clases/listas_de_listas_2/p3_dcccrossmusB.py
+++ clases/listas_de_listas_2/p3_dcccrossmusB.py
@@ -1,9 +1,7 @@
 def borrar_col(tablero, c):
-    numero = tablero[0][c]
-    for indice_f in range(len(tablero)):
-        for indice_c in range(len(tablero[0])):
-            if indice_c == c:
-                num = tablero[indice_f][indice_c]
-                if num > numero:
-                    tablero[indice_f][indice_c] = 0
+    total = tablero[0][c]
+    for indice_f in range(1, len(tablero)):
+        numero = tablero[indice_f][c]
+        if numero > total:
+            tablero[indice_f][c] = 0
     return tablero
