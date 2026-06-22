372911f66c32731324bc4c421692373caab9e567
ricardourvina <rurvinac@estudiante.uc.cl>
2026-06-21 23:40:06 -0400

    Clase 28 Ordenamiento II

--- clases/oop_sort/clase.py
+++ clases/oop_sort/clase.py
@@ -1,13 +1,18 @@
-resultados = [
-    ["Dora", 1200, 20],
-    ["Bob", 950, 25],
-    ["Homero", 1500, 45],
-    ["Lisa", 1500, 60]
-] # votos comuna San Joaquin
-
+lista = [
+     ["4","2"], # 8
+    ["1","3"], # 3
+     ["3","3"], #9
+    ["0","2"],# 0
+]
+# [ ["0","2"],["1","3"], ["4","2"], ["3","3"]]
+# ordenar segun la multiplicacion de sus elementos
 def criterio(item):
-    return item[1], -item[2]
+    return int(item[0]) * int(item[1])
+    # primer_num = int(item[0])
+    # segundo_num = int(item[1])
+    # return primer_num * segundo_num
 
-resultados.sort(key=criterio, reverse=True)
+# lista.sort(key=criterio)
+resultado = sorted(lista, key=criterio)
+print(lista)
 
-print(resultados)
